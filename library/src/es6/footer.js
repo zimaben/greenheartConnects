2bf6e18962a35f646e3571f1f0ace747853a15e4
ght-ben-osx <btoth@greenheart.org>
2020-09-03 21:25:08 +0800

    countdown

--- library/src/es6/footer.js
+++ library/src/es6/footer.js
@@ -3,6 +3,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     if( document.getElementById('home-hero') && document.getElementById('home-hero').classList.contains('loggedout') ){
         let data_target = document.getElementById( 'pleaselogin');
         window.location = 'https://greenheartconnects.org/login/?action=login';
+        
         //let body = document.getElementsByTagName('body')[0];
         ///if( ! body.classList.contains('modal-open') ){
             //can't call a modal from inside a modal
@@ -13,7 +14,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
         }
         */
     }
-    
+    doCountdowns();
     document.addEventListener("click", function(event){
         //if click isn't doing something else       
         if(event.target.onclick !== "function"){
@@ -29,3 +30,33 @@ window.addEventListener('DOMContentLoaded', (event) => {
     
     });
 });
+function doCountdowns(){
+    console.log('doing countdowns');
+    let countdowns = document.querySelectorAll('[data-seconds]');
+    for( let countdown of countdowns ){
+        console.log(countdown);
+        let seconds = countdown.dataset.seconds;
+        let cd = new theCountdown(seconds,'seconds',0,countdown);
+    }
+} 
+
+function theCountdown(input = '', format='seconds', offset = 0, target){
+    var x = setInterval(function() {
+        if(input > 1){
+            let diff = input;
+            let days = Math.floor( input / 86400 );
+            diff = diff - (days * 86400);
+            let hours = Math.floor(diff / 3600 );
+            diff = diff - (hours * 3600);
+            let minutes = Math.floor(diff / 60 );
+            let seconds = diff - (minutes * 60);
+            let string = (days) ? days + ' days, ' + hours + ':'+ minutes+':'+seconds : hours + ':'+ minutes+':'+seconds;
+            target.innerHTML = string;
+            input--;
+        } else {
+            clearInterval(x);
+            target.innerHTML = 'now';
+        }
+
+    },1000);
+}
